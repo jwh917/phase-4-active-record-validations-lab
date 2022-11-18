40b79375a4abad37622b5449f0f5007818cd764f
Julien Hnery <jwh_917@yahoo.com>
2022-11-17 21:28:25 -0500

    Active Record Validations Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+  # validate title_validator
+
+  # def title_validator
+  #   if title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
+  #     errors.add(:title, "must be clickbait")
+  # end
+
+  validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
